8a50f6334c64ae747c4c3e3b629a5205745e8592
Jun Shimura <j.shimura@sugawara.ac.jp>
2024-12-10 17:43:12 +0900

    HELLO

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 print("Hello, VS Code!")
-import cv2
-print(cv2.__version__)
+import sys
+print("Python executable:", sys.executable)
+print("Python version:", sys.version)
+
+
+# import cv2
+# print(cv2.__version__)
